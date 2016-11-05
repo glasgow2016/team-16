26e24d3eaac2e5d9db263527fe16dd683d6600e8
Stephanos Konstantinou <Stephanos Konstantinou>
2016-11-05 20:33:15 +0000

    *******

--- SpringBoot/Maggies/src/main/java/team16/LoginController.java
+++ SpringBoot/Maggies/src/main/java/team16/LoginController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-/*@RestController
+/*
+@RestController
 public class LoginController {
 
-    private static final String template = "Hello, %s!";
+   
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(method = RequestMethod.GET, name="/login")
